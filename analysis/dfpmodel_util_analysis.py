0b2474340f6315397b2ee4d93cbc78bbb7dbf5de
Kishore T <50635865+kishoret04@users.noreply.github.com>
2021-08-19 22:06:41 -0500

    Add files via upload

--- analysis/dfpmodel_util_analysis.py
+++ analysis/dfpmodel_util_analysis.py
@@ -36,7 +36,7 @@ class DynamicFPModel(pl.LightningModule):
         self.dev_perf   = {'vader': 0, 'flair': 0, 'sent': 0, 'subj': 0,'emotion': 0, 'mean': 0}
         self.test_perf  = {'vader': 0, 'flair': 0, 'sent': 0, 'subj': 0,'emotion': 0, 'mean': 0}
         self.naspect = 0
-        ###############kishore_update#######################################
+        ###############~_update#######################################
         ##BERT pretrained
         # import BERT-base pretrained model
         # Feed input to BERT/ELECTRA
@@ -468,7 +468,7 @@ class BertFPModel(pl.LightningModule):
         self.dev_perf   = {'vader': 0, 'flair': 0, 'sent': 0, 'subj': 0,'emotion': 0, 'mean': 0}
         self.test_perf  = {'vader': 0, 'flair': 0, 'sent': 0, 'subj': 0,'emotion': 0, 'mean': 0}
         self.naspect = 0
-        ###############kishore_update#######################################
+        ###############~_update#######################################
         ##BERT pretrained
         # import BERT-base pretrained model
         self.bert  = DistilBertModel.from_pretrained('distilbert-base-uncased')
